408049c93455a93d1c4781802b58b6bab5c49949
stevescott719 <steve.scott.719@gmail.com>
2025-11-14 00:34:40 -0600

    D: update Main and add CircleShape

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    pygame.time.Clock
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -21,6 +24,9 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
+
+        #print(f"Delta time: {dt:.4f} seconds")
 
 
 if __name__ == "__main__":
