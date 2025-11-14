64bfd7b66b4aa515cab7e84792b593b043ef569c
stevescott719 <steve.scott.719@gmail.com>
2025-11-14 00:09:58 -0600

    C: updated main to open a black screen

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
+from logger import log_state
 
 def main():
+    
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print(f"Screen width: " + str(SCREEN_WIDTH) + "\nScreen height: " + str(SCREEN_HEIGHT))
+    
+    pygame.init()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.QUIT
+                return
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
