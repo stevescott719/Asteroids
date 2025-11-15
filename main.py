e68dbeb824f77ec4bf3a5a39e0cac4e7ae282bfa
stevescott719 <steve.scott.719@gmail.com>
2025-11-14 23:38:56 -0600

    G: Lession 3 Groups

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)    
+
     dt = 0
 
     while True:
@@ -20,16 +27,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-
-        #print(f"Delta time: {dt:.4f} seconds")
-
 
 if __name__ == "__main__":
     main()
